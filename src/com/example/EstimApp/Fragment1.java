510bbdd7a40f11b8296cc64095d18e698fdcd421
kverhun <k.verhun@gmail.com>
2015-12-09 22:30:17 +0200

    Add Server class; add test login check

--- src/com/example/EstimApp/Fragment1.java
+++ src/com/example/EstimApp/Fragment1.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import com.example.EstimApp.Server.Server;
 
 /**
  * Created by Kostiantyn on 27.11.2015.
@@ -17,20 +19,37 @@ public class Fragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.layout1, container, false);
+        final View rootView = inflater.inflate(R.layout.layout1, container, false);
 
-        Button button = (Button)rootView.findViewById(R.id.layout1SwitchButton);
+        final Button button = (Button)rootView.findViewById(R.id.layout1SwitchButton);
         if (button != null)
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(getActivity(), Activity2.class);
-                startActivity(intent);
-
+                onSuccessLogin();
             }
+        });
+
+        final Button loginButton = (Button)rootView.findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                EditText loginEdit = (EditText)rootView.findViewById(R.id.editTextNickName);
+                String login = loginEdit.getText().toString();
+
+                Server server = new Server();
+                if (server.checkUsername(login))
+                    onSuccessLogin();
+            }
+
         });
 
         return rootView;
     }
+
+    private void onSuccessLogin(){
+        Intent intent = new Intent(getActivity(), Activity2.class);
+        startActivity(intent);
+    }
+
 }
---
+++ src/com/example/EstimApp/Server/Server.java
@@ -0,0 +1,11 @@
+package com.example.EstimApp.Server;
+
+/**
+ * Created by Kostya on 09.12.2015.
+ */
+public class Server {
+
+    public boolean checkUsername(String username){
+        return username.equals("admin");
+    }
+}
